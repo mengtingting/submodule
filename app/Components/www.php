4e0de1b9dda422765af691437810f815e996afd3
mengtingting <857334730@qq.com>
2019-02-15 14:30:46 +0800

    1
    1

--- app/Components/www.php
+++ app/Components/www.php
@@ -11,5 +11,18 @@ namespace App\Components;
 
 class www
 {
-
+    /*
+        * 将2017-11-27 00:00:00转换为2017年11月27日
+        *
+        * By TerryQi
+        *
+        * 2017-12-04
+        *
+        */
+    public static function getYMDChi($date_str)
+    {
+        $date_arr = explode(' ', $date_str);
+        $date_obj_arr = explode('-', $date_arr[0]);
+        return $date_obj_arr[0] . "年" . $date_obj_arr[1] . "月" . $date_obj_arr[2] . "日";
+    }
 }
